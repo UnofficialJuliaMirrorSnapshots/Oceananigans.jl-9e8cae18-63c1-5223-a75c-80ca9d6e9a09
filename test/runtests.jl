0958463490a39c43e1288647a4957866606de18e
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-24 12:41:13 +0000

    Snapshot of branch master taken on 2019-05-24T08:41:12.561-04:00 from "https://github.com/UnofficialJuliaMirror/Oceananigans.jl-9e8cae18-63c1-5223-a75c-80ca9d6e9a09"

--- test/runtests.jl
+++ test/runtests.jl
@@ -209,6 +209,13 @@ float_types = [Float32, Float64]
                 @test poisson_ppn_planned_div_free_cpu(ft, Nx, Ny, Nz, FFTW.ESTIMATE)
             end
         end
+
+        @testset "Analytic solution reconstruction" begin
+            println("    Testing analytic solution reconstruction...")
+            for N in [32, 48, 64], m in [1, 2, 3]
+                @test poisson_ppn_recover_sine_cosine_solution(Float64, N, N, N, 100, 100, 100, m, m, m)
+            end
+        end
     end
 
     @testset "Model" begin
@@ -233,6 +240,12 @@ float_types = [Float32, Float64]
         @testset "Adams-Bashforth 2" begin
             for arch in archs, ft in float_types
                 run_first_AB2_time_step_tests(arch, ft)
+            end
+        end
+
+        @testset "Incompressibility" begin
+            for Nt in [1, 10, 100]
+                @test incompressible_in_time(CPU(), Float64, Nt)
             end
         end
     end
@@ -302,4 +315,3 @@ float_types = [Float32, Float64]
         end
     end
 end # Oceananigans tests
-
